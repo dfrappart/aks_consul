90b3ac4f80d9fc9b7b14e0944ee0b6aad641f191
David Frappart <david@frappart.com>
2022-03-03 20:55:13 +0100

    adding ms sql configuration and fixing some option related to region

--- Terraformconfig/01_Infra/main.tf
+++ Terraformconfig/01_Infra/main.tf
@@ -58,6 +58,8 @@ module "SpokeVNet" {
   VNetSuffix                              = each.value.VNetSuffix
   IsBastionEnabled                        = each.value.IsBastionEnabled
   VNetAddressSpace                        = [each.value.VNetAddressSpace]
+  NetworkWatcherName                      = "NetworkWatcher_${module.ResourceGroup[0].RGLocation}"
+  IsTrafficAnalyticsEnabled               = false
   ResourceOwnerTag                        = var.ResourceOwnerTag
   CountryTag                              = var.CountryTag
   CostCenterTag                           = var.CostCenterTag
@@ -124,7 +126,7 @@ module "VMWin" {
   STALogId                                = data.azurerm_storage_account.STALog.id
   TargetRG                                = module.ResourceGroup[3].RGName
   TargetLocation                          = module.SpokeVNet[each.key].VNetFullOutput.location
-  TargetSubnetId                          = module.SpokeVNet[each.key].FESubnetFullOutput.id
+  TargetSubnetId                          = module.SpokeVNet[each.key].BESubnetFullOutput.id
   STABlobURI                              = data.azurerm_storage_account.STALog.primary_blob_endpoint
   VMSuffix                                = "${each.key}-${var.ResourcesSuffix}"
   VmSize                                  = "Standard_D2s_v3"
@@ -161,3 +163,151 @@ module "NSGRuleRDPAllow_FromBastionSpk2" {
 }
 
 */
+
+######################################################################
+# creating an azure sql server for lab usage
+
+
+# SQL Admin pwd to KV
+module "MSSQLAdminPWD" {
+
+  #Module Location
+  source                                  = "github.com/dfrappart/Terra-AZModuletest/Modules_building_blocks/412_KeyvaultSecret/"
+
+  #Module variable     
+  KeyVaultSecretSuffix                    = "MSSQLAdminPWD-${var.ResourcesSuffix}"
+  #PasswordValue                           = module.SecretTest.Result
+  KeyVaultId                              = data.azurerm_key_vault.keyvault.id
+  ResourceOwnerTag                        = var.ResourceOwnerTag
+  CountryTag                              = var.CountryTag
+  CostCenterTag                           = var.CostCenterTag
+  Environment                             = var.Environment
+  Project                                 = var.Project
+
+  depends_on = [
+
+  ]
+
+}
+
+# MS SQL Server
+resource "azurerm_mssql_server" "SQLAppServer1" {
+  name                                    = "mssql${var.ResourcesSuffix}"
+  resource_group_name                     = module.ResourceGroup[2].RGName
+  location                                = module.ResourceGroup[2].RGLocation
+  version                                 = var.MSSQLVer #"12.0"
+  administrator_login                     = "MSSQLAdminLogin-${var.ResourcesSuffix}"
+  administrator_login_password            = module.MSSQLAdminPWD.SecretFullOutput.value
+  minimum_tls_version                     = var.MSSQLTLSVersion #"1.2"
+
+  azuread_administrator {
+    login_username                        = "mssqlazureadadmin-${var.ResourcesSuffix}"
+    object_id                             = var.MSSQLADAdminObjectId
+  }
+
+  connection_policy                       = var.MSSQLConnectionPolicy
+
+  identity {
+    type                                  = var.MSSQLIdentityType
+    user_assigned_identity_ids            = var.MSSQLUAIId
+  }
+
+  public_network_access_enabled           = var.MSSQLPublicNetworkAccessEnabled
+
+  outbound_network_restriction_enabled    = var.MSSQLEgressRestrictionEnabled
+
+
+  tags                                    = var.DefaultTags
+}
+
+# MS SQL Server VNet rule
+resource "azurerm_mssql_virtual_network_rule" "VNetRule-SQLAppServer1" {
+  name                                    = "VNetRule-${azurerm_mssql_server.SQLAppServer1.name}"
+  server_id                               = azurerm_mssql_server.SQLAppServer1.id
+  subnet_id                               = module.SpokeVNet["Spoke1"].FESubnetFullOutput.id 
+}
+
+# MS SQL Server FW rule
+resource "azurerm_mssql_firewall_rule" "AllowAzureSVC" {
+  count            = var.MSSQLAcceptAzureService ? 1 : 0
+  name             = "AllowAzureSVC"
+  server_id        = azurerm_mssql_server.SQLAppServer1.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+# MSSQL RBAC Assignment to interact with storage account for logs
+resource "azurerm_role_assignment" "MSSQLSRVRBACForAudit" {
+  scope                = data.azurerm_storage_account.STALog.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_mssql_server.SQLAppServer1.identity.0.principal_id
+}
+
+# MS SQL Server Extended auditing policy
+resource "azurerm_mssql_server_extended_auditing_policy" "SQLAppServer1_ExtendedAuditingPolicy" {
+  storage_endpoint       = data.azurerm_storage_account.STALog.primary_blob_endpoint
+  server_id              = azurerm_mssql_server.SQLAppServer1.id
+  retention_in_days      = var.MSSQLSRVExtendedAutitingPolicyRetentionInDays
+  log_monitoring_enabled = var.MSSQLDBExtendedAutitingPolicyLogMonitoringEnabled
+
+
+  depends_on = [
+    azurerm_role_assignment.MSSQLSRVRBACForAudit,
+  ]
+}
+
+resource "azurerm_mssql_database" "SQLDBMyDrivingDB" {
+  name                                    = var.MSSQLDBName
+  server_id                               = azurerm_mssql_server.SQLAppServer1.id
+  auto_pause_delay_in_minutes             = var.MSSQLDBAutoPauseDelay
+  create_mode                             = var.MSSQLDBCreateMode
+  creation_source_database_id             = var.MSSQLDBCreationSrcId 
+  collation                               = var.MSSQLDBCollation
+  elastic_pool_id                         = var.MSSQLDBElasticPoolId
+  geo_backup_enabled                      = var.MSSQLDBGeoBackupEnabled
+  license_type                            = var.MSSQLDBLicenseType
+  long_term_retention_policy {
+    weekly_retention                      = var.MSSQLDBLongTermRentetionPolicyWeeklyRetention
+    monthly_retention                     = var.MSSQLDBLongTermRentetionPolicyMonthlyRetention
+    yearly_retention                      = var.MSSQLDBLongTermRentetionPolicyYearlyRetention
+    week_of_year                          = var.MSSQLDBLongTermRentetionPolicyWeekOfYear     
+  }
+
+  short_term_retention_policy {
+    retention_days                        = var.MSSQLDBShortTermRentetionPolicyRetentionDays   
+  }
+
+  max_size_gb                             = var.MSSQLDBMaxSize
+  min_capacity                            = var.MSSQLDBMinCapacity
+  restore_point_in_time                   = var.MSSQLDBRestorePoinInTime
+  recover_database_id                     = var.MSSQLDBRecoverDatabaseId
+  restore_dropped_database_id             = var.MSSQLDBRestoreDroppedDatabaseId
+  read_replica_count                      = var.MSSQLDBReadReplicaCount
+  read_scale                              = var.MSSQLDBReadScale
+  sku_name                                = var.MSSQLDBSku
+  storage_account_type                    = var.MSSQLDBStaType
+  zone_redundant                          = var.MSSQLDBZoneRedundant
+
+  threat_detection_policy {
+    state                                 = var.MSSQLDBThreatDetectionPolicyState
+    disabled_alerts                       = var.MSSQLDBThreatDetectionPolicyDisabledAlerts 
+    email_account_admins                  = var.MSSQLDBThreatDetectionPolicyEmailAccountAdmins
+    email_addresses                       = var.MSSQLDBThreatDetectionPolicyEmail
+    retention_days                        = var.MSSQLDBThreatDetectionPolicyRetentionDays
+    storage_account_access_key            = data.azurerm_storage_account.STALog.primary_access_key 
+    storage_endpoint                      = data.azurerm_storage_account.STALog.primary_blob_endpoint   
+
+  }
+
+  tags = var.DefaultTags
+
+}
+
+resource "azurerm_mssql_database_extended_auditing_policy" "SQLDBMyDrivingDBExtendedAutitingPolicy" {
+  database_id                             = azurerm_mssql_database.SQLDBMyDrivingDB.id
+  storage_endpoint                        = data.azurerm_storage_account.STALog.primary_blob_endpoint
+  storage_account_access_key              = data.azurerm_storage_account.STALog.primary_access_key
+  storage_account_access_key_is_secondary = var.MSSQLDBExtendedAutitingPolicySTAAccessKeyIsSecondary
+  retention_in_days                       = var.MSSQLDBExtendedAutitingPolicyRetentionInDays
+  log_monitoring_enabled                  = var.MSSQLDBExtendedAutitingPolicyLogMonitoringEnabled
+}
