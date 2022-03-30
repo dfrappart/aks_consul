4b145259125c512546106c02bdc81ea80da80368
DavidFrappart <david@frappart.com>
2022-03-30 15:15:58 +0200

    fix module for v3.x provider version

--- Modules/PaaS_Azure_Container_Registry/Outputs.tf
+++ Modules/PaaS_Azure_Container_Registry/Outputs.tf
@@ -6,66 +6,64 @@
 #Output for the module
 
 output "Name" {
-  value = azurerm_container_registry.ACR.name
-  description = "The name of the ACR"
+  value                           = azurerm_container_registry.ACR.name
+  description                     = "The name of the ACR"
 }
 
 
 output "RGName" {
-  value = azurerm_container_registry.ACR.resource_group_name
-  description = "The name of the RG containing the ACR"
+  value                           = azurerm_container_registry.ACR.resource_group_name
+  description                     = "The name of the RG containing the ACR"
 }
 
 
 output "Location" {
-  value = azurerm_container_registry.ACR.location
-  description = "The location of the ACR"
+  value                           = azurerm_container_registry.ACR.location
+  description                     = "The location of the ACR"
 }
 
 output "AdminEnabled" {
-  value = azurerm_container_registry.ACR.admin_enabled
-  description = "The status of the admin account state, enabled or not"
+  value                           = azurerm_container_registry.ACR.admin_enabled
+  description                     = "The status of the admin account state, enabled or not"
 }
 
 
 
 output "SKu" {
-  value = azurerm_container_registry.ACR.sku
-  description = "The ACR SKU"
+  value                           = azurerm_container_registry.ACR.sku
+  description                     = "The ACR SKU"
 }
 
-output "ReplicatedLocations" {
-  value = azurerm_container_registry.ACR.georeplication_locations
-  description = "The Region in which the ACR is replicated"
+output "ACRFull" {
+  value                           = azurerm_container_registry.ACR
+  description                     = "The Region in which the ACR is replicated"
+  sensitive                       = true
 }
 
 
 output "Id" {
-  value = azurerm_container_registry.ACR.id
-  description = "The Container Registry ID"
+  value                           = azurerm_container_registry.ACR.id
+  description                     = "The Container Registry ID"
 }
 
 
 output "ACRLoginServer" {
-  value = azurerm_container_registry.ACR.login_server
-  description = "The URL that can be used to log into the container registry"
+  value                           = azurerm_container_registry.ACR.login_server
+  description                     = "The URL that can be used to log into the container registry"
 }
 
 
 
 output "ACRAdminName" {
-  value = var.IsAdminEnabled ? azurerm_container_registry.ACR.admin_username : "Admin not enabled"
-  description = "The Region in which the ACR is replicated"
+  value                           = var.IsAdminEnabled ? azurerm_container_registry.ACR.admin_username : "Admin not enabled"
+  description                     = "The Admin name of the ACR"
 }
 
 
 
 output "ACRAdminPassword" {
-  value = var.IsAdminEnabled ? azurerm_container_registry.ACR.admin_password : "Admin not enabled"
-  description = "The Admin password of the ACR"
-  sensitive = true
+  value                           = var.IsAdminEnabled ? azurerm_container_registry.ACR.admin_password : "Admin not enabled"
+  description                     = "The Admin password of the ACR"
+  sensitive                       = true
 }
 
-output "test" {
-  value = local.test
-}
